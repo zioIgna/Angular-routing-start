f38cc10f8d4bf67080e24ecfebb75982c273a716
zioIgna <ignaziocarbonaro@hotmail.com>
2018-06-18 23:23:27 +0200

    11.133 Redirecting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,19 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },      // localhost:4200/users
-  { path: 'users', component: UsersComponent },      // localhost:4200/users
-  { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },     // localhost:4200/users
-  { path: 'servers/:id', component: ServerComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent }      // localhost:4200/users
+  { path: 'users', component: UsersComponent, children:[
+    { path: ':id/:name', component: UserComponent },
+  ]},      // localhost:4200/users
+  { path: 'servers', component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent },
+    { path: ':id/edit', component: EditServerComponent }
+  ]},
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -30,7 +35,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
